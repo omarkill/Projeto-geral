3b603f819374987246604c248ebdeed2c7e5b1da
NeoArraze <75584023+omarkill@users.noreply.github.com>
2021-05-08 15:19:42 -0300

    only test

---
+++ passwordgenerationtest1.py
@@ -0,0 +1,25 @@
+Alfa1=['a', 'e', 'i', 'o', 'u',]
+
+Alfa2=('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'w', 'y', 'z')
+
+Alfa3=('cha','che','chi','cho','chu','nha','nhe','nhi','nho','nhu','rra','rre','rri','rro','rru','ssa','sse','ssi','sso','ssu','qua','que','qui','quo')
+
+Nums=list('0123456789')
+
+
+for i1 in (Alfa2):
+    for i2 in (Alfa2):
+        for i3 in (Alfa2):
+            for i4 in (Alfa2):
+                for i5 in (Alfa2):
+                    for i6 in (Alfa2):
+                        for i7 in (Alfa2):
+                            for i8 in (Alfa2):
+                                for i9 in (Alfa2):
+                                 y=(i1+i2+i3+i4+i5+i6+i7+i8+i9)
+                    print(y)
+
+
+                    
+
+
